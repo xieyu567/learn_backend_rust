5b7ecb1d398efbb991aba550d2fb4db0528ba5ff
xieyu567 <xieyu345800@sina.com>
2022-10-16 21:47:48 +0800

    slim docker file

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,24 @@
-FROM rust:1.64.0
+FROM lukemathwalker/cargo-chef:latest-rust-1.64.0 as chef
+WORKDIR /app
+RUN apt update && apt install lld clang -y
+
+FROM chef as planner
+COPY . .
+RUN cargo chef prepare --recipe-path recipe.json
+
+FROM chef AS builder
 
 WORKDIR /app
 RUN apt update && apt install lld clang -y
 COPY . .
 RUN cargo build --release
 
-ENTRYPOINT ["./target/release/learn_backend_rust"]
+FROM debian:bullseye-slim AS runtime
+
+WORKDIR /app
+RUN apt-get update -y && apt-get install -y --no-install-recommends openssl ca-certificates && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /app/target/release/learn_backend_rust learn_backend_rust
+COPY configuration configuration
+ENV APP_ENVIRONMENT production
+
+ENTRYPOINT ["./learn_backend_rust"]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use learn_backend_rust::configuration::get_configuration;
 use learn_backend_rust::startup::run;
 use learn_backend_rust::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -11,12 +10,14 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to CockroachDB");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
     run(listener, connection_pool)?.await?;
     Ok(())
 }
